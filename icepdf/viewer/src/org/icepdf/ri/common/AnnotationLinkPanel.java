60b3f94066188376cf23deeac48be4f03609de65
Patrick Corless <pcorless@gmail.com>
2009-11-19 19:09:54 +0000

    PDF-74 - Message internationalization and a callback to SwingController in the new AnnotationLinkPanel

--- icepdf/viewer/src/org/icepdf/ri/common/AnnotationLinkPanel.java
+++ icepdf/viewer/src/org/icepdf/ri/common/AnnotationLinkPanel.java
@@ -4,12 +4,16 @@ import org.icepdf.core.views.swing.AnnotationComponent;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.ResourceBundle;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.border.EtchedBorder;
 
 public class AnnotationLinkPanel extends JPanel {
+    private SwingController controller;
+    private ResourceBundle messageBundle;
+
     private AnnotationComponent annotationComponent;
 
     private JButton colorButton;
@@ -17,10 +21,14 @@ public class AnnotationLinkPanel extends JPanel {
     private JTextField pageField;
     private JLabel pageLabel;
 
-    public AnnotationLinkPanel() {
-        super(new FlowLayout(FlowLayout.CENTER, 0, 0));
+    public AnnotationLinkPanel(SwingController controller) {
+        super(new FlowLayout(FlowLayout.CENTER, 0, 0), true);
+
+        this.controller = controller;
+        this.messageBundle = this.controller.getMessageBundle();
 
         // Setup the basics of the panel
+        setFocusable(true);
         setBorder(new EmptyBorder(10, 5, 1, 5));
 
         // Add the tabbed pane to the overall panel
@@ -96,24 +104,24 @@ public class AnnotationLinkPanel extends JPanel {
         // Create and setup an Appearance panel
         JPanel appearancePane = new JPanel(new GridLayout(5, 2, 5, 5));
         appearancePane.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED),
-                                                  "Appearance",
+                                                  messageBundle.getString("viewer.utilityPane.link.appearanceTitle"),
                                                   TitledBorder.LEFT,
                                                   TitledBorder.DEFAULT_POSITION));
 
         linkTypeBox = new JComboBox(new String[] {"Invisible Rectangle"});
-        appearancePane.add(new JLabel("Link Type:"));
+        appearancePane.add(new JLabel(messageBundle.getString("viewer.utilityPane.link.linkType")));
         appearancePane.add(linkTypeBox);
 
         linkStyleBox = new JComboBox(new String[] {"Solid"});
-        appearancePane.add(new JLabel("Line Style:"));
+        appearancePane.add(new JLabel(messageBundle.getString("viewer.utilityPane.link.lineStyle")));
         appearancePane.add(linkStyleBox);
 
         linkThicknessBox = new JComboBox(new String[] {"Thin"});
-        appearancePane.add(new JLabel("Link Thickness:"));
+        appearancePane.add(new JLabel(messageBundle.getString("viewer.utilityPane.link.lineThickness")));
         appearancePane.add(linkThicknessBox);
 
         linkHighlightBox = new JComboBox(new String[] {"Invert"});
-        appearancePane.add(new JLabel("Highlight Style:"));
+        appearancePane.add(new JLabel(messageBundle.getString("viewer.utilityPane.link.highlightStyle")));
         appearancePane.add(linkHighlightBox);
 
         colorButton = new JButton();
@@ -121,7 +129,7 @@ public class AnnotationLinkPanel extends JPanel {
             public void actionPerformed(ActionEvent event) {
                 Color chosenColor =
                         JColorChooser.showDialog(colorButton,
-                                                 "Annotation Link - Color",
+                                                 messageBundle.getString("viewer.utilityPane.link.colorChooserTitle"),
                                                  colorButton.getBackground());
                 if (chosenColor != null) {
                     colorButton.setBackground(chosenColor);
@@ -131,7 +139,7 @@ public class AnnotationLinkPanel extends JPanel {
             }
         });
         colorButton.setBackground(Color.RED);
-        appearancePane.add(new JLabel("Color:"));
+        appearancePane.add(new JLabel(messageBundle.getString("viewer.utilityPane.link.colorLabel")));
         appearancePane.add(colorButton);
 
         return appearancePane;
@@ -141,28 +149,28 @@ public class AnnotationLinkPanel extends JPanel {
         // Create and setup an Action panel
         JPanel pageNumberSubpane = new JPanel(new GridLayout(2, 3, 5, 5));
         pageNumberSubpane.setBorder(new EmptyBorder(0, 40, 0, 0));
-        pageNumberSubpane.add(new JLabel("Page:"));
+        pageNumberSubpane.add(new JLabel(messageBundle.getString("viewer.utilityPane.link.pageLabel")));
         pageField = new JTextField();
         pageNumberSubpane.add(pageField);
-        pageLabel = new JLabel(generatePageLabelText(-1));
+        pageLabel = new JLabel(generatePageLabelText());
         pageNumberSubpane.add(pageLabel);
-        pageNumberSubpane.add(new JLabel("Zoom:"));
+        pageNumberSubpane.add(new JLabel(messageBundle.getString("viewer.utilityPane.link.zoomLabel")));
         zoomBox = new JComboBox(new String[] {"Fit Page"});
         pageNumberSubpane.add(zoomBox);
 
         JPanel pageNumberPane = new JPanel(new BorderLayout(5, 5));
-        JRadioButton pageNumberRadio = new JRadioButton("Use Page Number", true);
+        JRadioButton pageNumberRadio = new JRadioButton(messageBundle.getString("viewer.utilityPane.link.usePage"), true);
         pageNumberPane.add(pageNumberRadio, BorderLayout.NORTH);
         pageNumberPane.add(pageNumberSubpane, BorderLayout.CENTER);
 
         JPanel namedDestSubpane = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5));
         namedDestSubpane.setBorder(new EmptyBorder(0, 40, 0, 0));
-        namedDestSubpane.add(new JLabel("Name:"));
+        namedDestSubpane.add(new JLabel(messageBundle.getString("viewer.utilityPane.link.nameLabel")));
         namedDestSubpane.add(new JLabel("X"));
-        namedDestSubpane.add(new JButton("Browse..."));
+        namedDestSubpane.add(new JButton(messageBundle.getString("viewer.utilityPane.link.browse")));
 
         JPanel namedDestPane = new JPanel(new BorderLayout(5, 5));
-        JRadioButton namedDestRadio = new JRadioButton("Use Named Destination", false);
+        JRadioButton namedDestRadio = new JRadioButton(messageBundle.getString("viewer.utilityPane.link.useDestination"), false);
         namedDestPane.add(namedDestRadio, BorderLayout.NORTH);
         namedDestPane.add(namedDestSubpane, BorderLayout.CENTER);
 
@@ -172,7 +180,7 @@ public class AnnotationLinkPanel extends JPanel {
 
         JPanel actionPane = new JPanel(new GridLayout(2, 1, 2, 2));
         actionPane.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED),
-                                              "Actions",
+                                              messageBundle.getString("viewer.utilityPane.link.actionsTitle"),
                                               TitledBorder.LEFT,
                                               TitledBorder.DEFAULT_POSITION));
         actionPane.add(pageNumberPane);
@@ -181,9 +189,10 @@ public class AnnotationLinkPanel extends JPanel {
         return actionPane;
     }
 
-    private String generatePageLabelText(int totalPage) {
-        if (totalPage > -1) {
-            return "of " + totalPage;
+    private String generatePageLabelText() {
+        if ((controller != null) &&
+            (controller.getDocument() != null)) {
+            return "of " + controller.getDocument().getNumberOfPages();
         }
 
         return "of ?";
