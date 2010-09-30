b451d7d6438d333ff8d68c30a8465f5d4c9a91a5
Patrick Corless <pcorless@gmail.com>
2010-09-30 12:03:14 +0000

    PDF-206 applyed patch that improves font family detection.

--- icepdf/core/src/org/icepdf/core/util/FontUtil.java
+++ icepdf/core/src/org/icepdf/core/util/FontUtil.java
@@ -33,19 +33,34 @@
 package org.icepdf.core.util;
 
 /**
- * Font utility conatin a bunch of commonly used font utitlity methods.
+ * Font utility contians a bunch of commonly used font utility methods.
  *
  * @since 3.1
  */
 public class FontUtil {
 
+    // awt font style lookup style tokens
+    private static final String AWT_STYLE_BOLD_ITAL = "boldital";
+    private static final String AWT_STYLE_DEMI_ITAL = "demiital";
+    private static final String AWT_STYLE_ITAL = "ital";
+    private static final String AWT_STYLE_OBLI = "obli";
+
+    // Font style names used to derive family names.
+    private static final String STYLE_BOLD_ITALIC = "bolditalic";
+    private static final String STYLE_DEMI_ITALIC = "demiitalic";
+    private static final String STYLE_BOLD = "bold";
+    private static final String STYLE_DEMI = "demi";
+    private static final String STYLE_ITALIC = "italic";
+    private static final String STYLE_BLACK = "black";
+
     /**
-     * Utitility method which maps know sytle strings to an AWT font style constants.  The style
-     * attribute read as follows fron the java.awt.font constructor:
+     * Utility method which maps know style strings to an AWT font style constants.
+     * The style attribute read as follows from the java.awt.font constructor:
      * <ul>
-     *   the style constant for the Font The style argument is an integer bitmask that may be PLAIN, or a bitwise
-     *   union of BOLD and/or ITALIC (for example, ITALIC or BOLD|ITALIC). If the style argument does not conform to
-     *   one of the expected integer bitmasks then the style is set to PLAIN.
+     *   the style constant for the Font The style argument is an integer bitmask
+     *   that may be PLAIN, or a bitwise union of BOLD and/or ITALIC
+     *   (for example, ITALIC or BOLD|ITALIC). If the style argument does not
+     *   conform to one of the expected integer bitmasks then the style is set to PLAIN.
      * </ul>
      *
      * @param name base name of font.
@@ -54,11 +69,15 @@ public class FontUtil {
     public static int guessAWTFontStyle(String name) {
         name = name.toLowerCase();
         int decorations = 0;
-        if ((name.indexOf("boldital") > 0) || (name.indexOf("demiital") > 0)) {
+        if (name.indexOf(AWT_STYLE_BOLD_ITAL) > 0 ||
+                name.indexOf(AWT_STYLE_DEMI_ITAL) > 0) {
             decorations |= java.awt.Font.BOLD | java.awt.Font.ITALIC;
-        } else if (name.indexOf("bold") > 0 || name.indexOf("black") > 0 || name.indexOf("demi") > 0) {
+        } else if (name.indexOf(STYLE_BOLD) > 0 ||
+                name.indexOf(STYLE_BLACK) > 0 ||
+                name.indexOf(STYLE_DEMI) > 0) {
             decorations |= java.awt.Font.BOLD;
-        } else if (name.indexOf("ital") > 0 || name.indexOf("obli") > 0) {
+        } else if (name.indexOf(AWT_STYLE_ITAL) > 0 ||
+                name.indexOf(AWT_STYLE_OBLI) > 0) {
             decorations |= java.awt.Font.ITALIC;
         } else {
             decorations |= java.awt.Font.PLAIN;
@@ -67,7 +86,17 @@ public class FontUtil {
     }
 
     /**
-     * <p>Utility method for guessing a font family name from its base name.</p>
+     * Utility method for guessing a font family name from its base name. Font
+     * names are usually made up of a familyName followed by a style
+     * name.  For example:
+     * <p/>
+     * <ul>
+     *    <li>Arial,BoldItalic</li>
+     *    <li>Times-Bold"</li>
+     *    <li>Arial BoldItalic</li>
+     *    <li>TimesNewRomansBold</li>
+     * </ul>
+     *
      *
      * @param name base name of font.
      * @return guess of the base fonts name.
@@ -81,6 +110,19 @@ public class FontUtil {
         // Family name usually precedes a dash, example "Times-Bold",
         if ((inx = fam.lastIndexOf('-')) > 0)
             fam = fam.substring(0, inx);
+        // Family name with no dash or commas, example "TimesNewRomansBold" or
+        // "CalibriBoldItalic"
+        if ((inx = fam.toLowerCase().lastIndexOf(STYLE_BOLD_ITALIC)) > 0){
+        	fam = fam.substring(0, inx);
+        }else if ((inx = fam.toLowerCase().lastIndexOf(STYLE_DEMI_ITALIC)) > 0){
+        	fam = fam.substring(0, inx);
+        }else if ((inx = fam.toLowerCase().lastIndexOf(STYLE_BOLD)) > 0){
+        	fam = fam.substring(0, inx);
+        }else if ((inx = fam.toLowerCase().lastIndexOf(STYLE_ITALIC)) > 0){
+        	fam = fam.substring(0, inx);
+        }else if ((inx = fam.toLowerCase().lastIndexOf(STYLE_BLACK)) > 0){
+        	fam = fam.substring(0, inx);
+        }
         return fam;
     }
 
