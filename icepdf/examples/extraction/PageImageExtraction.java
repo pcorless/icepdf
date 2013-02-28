173aa7fb49133969bbbd14d356f23cab1be8fd7c
Patrick Corless <pcorless@gmail.com>
2013-02-28 18:35:35 +0000

    PDF-521 updated page image extraction to use two thread to aid in the extraction time.

--- icepdf/examples/extraction/PageImageExtraction.java
+++ icepdf/examples/extraction/PageImageExtraction.java
@@ -17,7 +17,6 @@ import org.icepdf.core.exceptions.PDFException;
 import org.icepdf.core.exceptions.PDFSecurityException;
 import org.icepdf.core.pobjects.Document;
 import org.icepdf.core.pobjects.Page;
-import org.icepdf.core.util.Defs;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -26,31 +25,53 @@ import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * The <code>PageImageExtraction</code> class is an example of how to extract
  * images from a PDF document.  A file specified at the command line is opened
- * and any images that are embedded in the first page's content are
- * saved to disk as PNG graphic files.
+ * and any images that are embedded in a documents page is written to file.
  *
- * @since 2.0
+ * @since 5.0
  */
 public class PageImageExtraction {
 
     public static void main(String[] args) {
 
-        // setup the memory manager to avoid keeping a lot of data around
-        Defs.setProperty("org.icepdf.core.maxSize", 1);
-        Defs.setProperty("org.icepdf.core.scaleImages", "false");
-
         // Get a file from the command line to open
         String filePath = args[0];
 
+        PageImageExtraction pageImageExtraction = new PageImageExtraction();
+
+        pageImageExtraction.pageImageExtraction(filePath);
+    }
+
+    public void pageImageExtraction(String filePath) {
         // open the url
         Document document = new Document();
+
+        // setup two threads to handle image extraction.
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         try {
             document.setFile(filePath);
+            // create a list of callables.
+            int pages = document.getNumberOfPages();
+            List<Callable<Void>> callables = new ArrayList<Callable<Void>>(pages);
+            for (int i = 0; i <= pages; i++) {
+                callables.add(new CapturePageImages(document, i));
+            }
+            executorService.invokeAll(callables);
+
+            executorService.submit(new DocumentCloser(document)).get();
+        } catch (InterruptedException e) {
+            System.out.println("Error parsing PDF document " + e);
+        } catch (ExecutionException e) {
+            System.out.println("Error parsing PDF document " + e);
         } catch (PDFException ex) {
             System.out.println("Error parsing PDF document " + ex);
         } catch (PDFSecurityException ex) {
@@ -60,36 +81,62 @@ public class PageImageExtraction {
         } catch (IOException ex) {
             System.out.println("Error handling PDF document " + ex);
         }
+        executorService.shutdown();
+    }
 
-        // Get images from the first page of the document, asuming that there
-        // is at least one image to extract.
-        try {
-            int count = 0;
+    /**
+     * Captures images found in a page  parse to file.
+     */
+    public class CapturePageImages implements Callable<Void> {
+        private Document document;
+        private int pageNumber;
 
-            Page currentPage;
-            List<Image> images;
-            RenderedImage rendImage;
-            for (int i = 0, max = document.getNumberOfPages(); i < max; i++) {
-                currentPage = document.getPageTree().getPage(i);
+        private CapturePageImages(Document document, int pageNumber) {
+            this.document = document;
+            this.pageNumber = pageNumber;
+        }
 
-                images = currentPage.getImages();
-                for (Image image: images) {
+        public Void call() {
+            try {
+                Page currentPage = document.getPageTree().getPage(pageNumber);
+                int count = 0;
+                RenderedImage rendImage;
+                List<Image> images = currentPage.getImages();
+                for (Image image : images) {
                     count++;
                     if (image != null) {
                         rendImage = (BufferedImage) image;
-                        File file = new File("imageCapture1_" + count + ".png");
+                        System.out.println("Capture page " + pageNumber + " image " + count);
+                        File file = new File("imageCapture_" + pageNumber + "_" + count + ".png");
                         ImageIO.write(rendImage, "png", file);
                         image.flush();
                     }
                 }
                 // clears most resource.
                 images.clear();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            return null;
+        }
+    }
 
-            // clean up resources
-            document.dispose();
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * Disposes the document.
+     */
+    public class DocumentCloser implements Callable<Void> {
+        private Document document;
+
+        private DocumentCloser(Document document) {
+            this.document = document;
+        }
+
+        public Void call() {
+            if (document != null) {
+                document.dispose();
+                System.out.println("Document disposed");
+            }
+            return null;
         }
     }
 }
