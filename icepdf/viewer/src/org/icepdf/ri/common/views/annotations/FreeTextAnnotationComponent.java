db2f2266e547048de54439374f3c79dc2c1ce65d
Patrick Corless <pcorless@gmail.com>
2014-01-29 17:29:11 +0000

    PDF-691 updated freeText annotation component to respect the read only property on the annotations and now will prevent edits from ocuring.

--- icepdf/viewer/src/org/icepdf/ri/common/views/annotations/FreeTextAnnotationComponent.java
+++ icepdf/viewer/src/org/icepdf/ri/common/views/annotations/FreeTextAnnotationComponent.java
@@ -223,7 +223,7 @@ public class FreeTextAnnotationComponent extends MarkupAnnotationComponent
         } else if ("focusOwner".equals(prop) &&
                 newValue instanceof JTextArea) {
             JTextArea freeText = (JTextArea) newValue;
-            if (freeText.equals(freeTextPane)) {
+            if (freeText.equals(freeTextPane) && !annotation.getFlagReadOnly()) {
                 freeText.setEditable(true);
                 if (freeText instanceof ScalableTextArea) {
                     ((ScalableTextArea) freeText).setActive(true);
