66bdbdefe032c0f908effede5726271a7c48be11
Patrick Corless <pcorless@gmail.com>
2016-11-30 19:54:29 +0000

    PDF-1046 applied patch: addition of font properties dialog to Viewer RI showing all document font properties and provides a mechanism to allow for easy reset of the font cache properties file.

--- icepdf/core/src/org/icepdf/core/pobjects/fonts/FontManager.java
+++ icepdf/core/src/org/icepdf/core/pobjects/fonts/FontManager.java
@@ -798,8 +798,9 @@ public class FontManager {
 
                     if (found) {
                         if (logger.isLoggable(Level.FINER)) {
-                            logger.finer("Match Found for: " + fontName + ":" + getFontStyle(style, 0).trim() +
-                                    " Substituting " + baseName + ":" + path);
+                            logger.finer("----> Found font: " + baseName +
+                                    " family: " + getFontStyle(style, 0) +
+                                    " for: " + fontName + " " + path);
                         }
                         font = buildFont((String) fontData[3]);
                         // make sure the font does indeed exist
@@ -901,28 +902,21 @@ public class FontManager {
         int decorations = guessFontStyle(fontName);
         fontName = FontUtil.normalizeString(fontName);
         FontFile font;
-
-        // read font flags as it can sometimes give us hints as to serif
-        // san sarif or a monospace font, there is more data we can pull if needed too.
-        boolean isFixedPitch = (flags & org.icepdf.core.pobjects.fonts.Font.FONT_FLAG_FIXED_PITCH) != 0;
-        boolean isSerif = (flags & org.icepdf.core.pobjects.fonts.Font.FONT_FLAG_SERIF) != 0;
-//        boolean isSymbolic = (flags & org.icepdf.core.pobjects.fonts.Font.FONT_FLAG_SYMBOLIC) != 0;
-//        boolean isNotSymbolic = (flags & org.icepdf.core.pobjects.fonts.Font.FONT_FLAG_NON_SYMBOLIC) != 0;
         // If no name are found then match against the core java font names
         // "Serif", java equivalent is  "Lucida Bright"
-        if (fontName.contains("timesnewroman") ||
+        if ((fontName.contains("timesnewroman") ||
                 fontName.contains("bodoni") ||
                 fontName.contains("garamond") ||
                 fontName.contains("minionweb") ||
                 fontName.contains("stoneserif") ||
                 fontName.contains("georgia") ||
-                fontName.contains("bitstreamcyberbit")) {
+                fontName.contains("bitstreamcyberbit"))) {
             // important, add style information
             font = findFont(fontList, "lucidabright-" + getFontStyle(decorations, flags), 0);
         }
         // see if we working with a monospaced font, we sub "Sans Serif",
         // java equivalent is "Lucida Sans"
-        else if (fontName.contains("helvetica") ||
+        else if ((fontName.contains("helvetica") ||
                 fontName.contains("arial") ||
                 fontName.contains("trebuchet") ||
                 fontName.contains("avantgardegothic") ||
@@ -933,32 +927,26 @@ public class FontManager {
                 fontName.contains("gillsans") ||
                 fontName.contains("akzidenz") ||
                 fontName.contains("frutiger") ||
-                fontName.contains("grotesk")) {
+                fontName.contains("grotesk"))) {
             // important, add style information
             font = findFont(fontList, baseFontName + "-" + getFontStyle(decorations, flags), 0);
         }
         // see if we working with a mono spaced font "Mono Spaced"
         // java equivalent is "Lucida Sans Typewriter"
-        else if (fontName.contains("courier") ||
+        else if ((fontName.contains("courier") ||
                 fontName.contains("couriernew") ||
                 fontName.contains("prestige") ||
-                fontName.contains("eversonmono")) {
+                fontName.contains("eversonmono"))) {
             // important, add style information
             font = findFont(fontList, baseFontName + "typewriter-" + getFontStyle(decorations, flags), 0);
         }
         // first try get the first match based on the style type and finally on failure
         // failure go with the serif as it is the most common font family
         else {
-            if (isSerif) {
-                font = findFont(fontList, "lucidabright-" + getFontStyle(decorations, flags), 0);
-            } else if (isFixedPitch) {
-                // lucidatypewriter, seems to make the font engine barf, converting to other
-                // common fixed pitch font courier-new.
-                font = findFont(fontList, "couriernew-" + getFontStyle(decorations, flags), 0);
-            } else {
-                // sans serif
-                font = findFont(fontList, "lucidasans-" + getFontStyle(decorations, flags), 0);
-            }
+//            System.out.println("Decorations " + decorations);
+//            System.out.println("flags " + flags);
+//            System.out.println("sytel " + getFontStyle(decorations, flags));
+            font = findFont(fontList, "lucidabright-" + getFontStyle(decorations, flags), 0);
         }
 
         return font;
@@ -1174,11 +1162,9 @@ public class FontManager {
         String style = "";
         if ((sytle & BOLD_ITALIC) == BOLD_ITALIC) {
             style += " BoldItalic";
-        } else if ((sytle & BOLD) == BOLD ||
-                (flags & org.icepdf.core.pobjects.fonts.Font.FONT_FLAG_FORCE_BOLD) != 0) {
+        } else if ((sytle & BOLD) == BOLD) {
             style += " Bold";
-        } else if ((sytle & ITALIC) == ITALIC ||
-                (flags & org.icepdf.core.pobjects.fonts.Font.FONT_FLAG_FORCE_BOLD) != 0) {
+        } else if ((sytle & ITALIC) == ITALIC) {
             style += " Italic";
         } else if ((sytle & PLAIN) == PLAIN) {
             style += " Plain";
