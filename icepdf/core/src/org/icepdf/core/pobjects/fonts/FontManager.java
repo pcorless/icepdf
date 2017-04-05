411dd37f9c1dac7aa73d52de115a4c168dfed994
Patrick Corless <pcorless@gmail.com>
2017-04-05 16:24:01 +0000

    PDF-1112 reworked font assignment mechanism when dealing with free text annotations,  also fixed a potential initialization issue which might keeps changes from persisting.

--- icepdf/core/src/org/icepdf/core/pobjects/fonts/FontManager.java
+++ icepdf/core/src/org/icepdf/core/pobjects/fonts/FontManager.java
@@ -18,10 +18,8 @@ package org.icepdf.core.pobjects.fonts;
 import org.icepdf.core.util.Defs;
 import org.icepdf.core.util.FontUtil;
 
-import java.awt.Font;
 import java.awt.*;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.security.AccessControlException;
 import java.util.*;
@@ -201,8 +199,7 @@ public class FontManager {
     /**
      * <p>Initializes the fontList by reading the system fonts paths via readSystemFonts()
      * but only if the fontList is null or is empty.  Generally the fontManager
-     * is used with the {@link org.icepdf.ri.util.FontPropertiesManager }
-     * </p>
+     * is used with the org.icepdf.ri.util.FontPropertiesManager
      *
      * @return instance of the singleton fontManager.
      */
@@ -216,7 +213,7 @@ public class FontManager {
     /**
      * <p>Gets a Properties object containing font information for the operating
      * system which the FontManager is running on.  This Properties object
-     * can be saved to disk and read at a later time using the {@see #setFontProperties}
+     * can be saved to disk and read at a later time using the {@link #setFontProperties}
      * method.</p>
      *
      * @return Properties object containing font data information.
@@ -575,7 +572,7 @@ public class FontManager {
      * Reads the specified resources from the specified package.  This method
      * is intended to aid in the packaging of fonts used for font substitution
      * and avoids the need to install fonts on the client operating system.
-     * <p/>
+     * <br>
      * The following font resource types are supported are support:
      * <ul>
      * <li>TrueType - *.ttf, *.dfont, *.ttc</li>
@@ -789,7 +786,7 @@ public class FontManager {
                         found = true;
                     }
                     // symbol type fonts don't have an associated style, so
-                    // no point trying to match  them based on style. 
+                    // no point trying to match  them based on style.
                     else if (baseName.contains("wingdings") ||
                             baseName.contains("zapfdingbats") ||
                             baseName.contains("dingbats") ||
@@ -1058,80 +1055,13 @@ public class FontManager {
      * @return a valid font if found, null otherwise
      */
     private java.awt.Font findAWTFont(String fontName) {
-
         java.awt.Font font = null;
-        // references for system font list.
-        Object[] fontData;
-        String baseName;
-        String familyName;
-        // normalize the fontName we are trying to find a match for
-        int decorations = guessFontStyle(fontName);
-        String name = FontUtil.normalizeString(fontName);
-        int style;
-
-        if (fontList != null) {
-            for (int i = fontList.size() - 1; i >= 0; i--) {
-                fontData = fontList.get(i);
-                baseName = (String) fontData[0];
-                familyName = (String) fontData[1];
-                if (logger.isLoggable(Level.FINEST)) {
-                    logger.finest(baseName + " : " + familyName + "  : " + name);
-                }
-                if (name.contains(familyName) ||
-                        fontName.toLowerCase().contains(baseName)) {
-                    style = (Integer) fontData[2];
-                    boolean found = false;
-                    // ignore this font, as the cid mapping are not correct, or ther is
-                    // just look and feel issues with them.
-                    if (baseName.equals("opensymbol") ||
-                            baseName.equals("starsymbol")
-                            || baseName.equals("arial-black")
-                            || baseName.equals("arial-blackitalic")
-                            || baseName.equals("new")
-                            // mapping issue with standard ascii, not sure why, TimesNewRomanPSMT is ok.
-                            || baseName.equals("timesnewromanps")
-                            ) {
-                        //found = false;
-                    } else if (((decorations & BOLD_ITALIC) == BOLD_ITALIC) &&
-                            ((style & BOLD_ITALIC) == BOLD_ITALIC)) {
-                        found = true;
-                    } else if (((decorations & BOLD) == BOLD) &&
-                            ((style & BOLD) == BOLD)) {
-                        found = true;
-                    } else if (((decorations & ITALIC) == ITALIC) &&
-                            ((style & ITALIC) == ITALIC)) {
-                        found = true;
-                    } else if (((decorations & PLAIN) == PLAIN) &&
-                            ((style & PLAIN) == PLAIN)) {
-                        found = true;
-                    }
-                    // symbol type fonts don't have an associated style, so
-                    // no point trying to match  them based on style.
-                    else if (baseName.contains("wingdings") ||
-                            baseName.contains("zapfdingbats") ||
-                            baseName.contains("symbol")) {
-                        found = true;
-                    }
-
-                    if (found) {
-                        if (logger.isLoggable(Level.FINER)) {
-                            logger.finer("----> Found font: " + baseName +
-                                    " family: " + getFontStyle(style, 0) +
-                                    " for: " + fontName);
-                        }
-                        try {
-                            font = java.awt.Font.createFont(Font.TRUETYPE_FONT,
-                                    new File((String) fontData[3]));
-                        } catch (FontFormatException e) {
-                            logger.log(Level.FINE, "Error create new font", e);
-                        } catch (IOException e) {
-                            logger.log(Level.FINE, "Error reading font", e);
-                        }
-                        // make sure the font does indeed exist
-                        if (font != null) {
-                            break;
-                        }
-                    }
+        // get a list of all the fonts AWT has
+        java.awt.Font[] awtFonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        if (awtFonts != null) {
+            for (java.awt.Font awtFont : awtFonts) {
+                if (awtFont.getName().equals(fontName) || awtFont.getFamily().equals(fontName)) {
+                    return awtFont;
                 }
             }
         }
