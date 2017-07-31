9dc8b38a09ae99958425f12d09ed8d40078c76ad
Patrick Corless <pcorless@gmail.com>
2017-07-31 19:27:14 +0000

    PDF-1175 addition of smoothScaledImageReference fallback if the image isn't touched by the blurring code.

--- icepdf/core/core-awt/src/main/java/org/icepdf/core/pobjects/graphics/BlurredImageReference.java
+++ icepdf/core/core-awt/src/main/java/org/icepdf/core/pobjects/graphics/BlurredImageReference.java
@@ -18,14 +18,18 @@ import java.util.logging.Logger;
  * BlurredImageReference graph attempts to smooth out images using convolution.  The implementation takes advantage of
  * the java.awt.image.Kernel class to apply the matrix that defines the convolution.  The default blur factor is 3 which
  * seems to provide a nice balance,  the value can go as low as 2 for just a light blur or as high as 25 for a very
- * blurred image result.  This image refernce format is best suited for TIFF images that are hard to read at small zoom
+ * blurred image result.  This image reference format is best suited for TIFF images that are hard to read at small zoom
  * levels.
- * <p>
+ *
  * The blur value can be set with the system property org.icepdf.core.imageReference.blurred.dimension and once again
  * the default value is 3.  The blurring algorithm has a minimum image size associated with it.  To small an image and
  * the blur effect looks less then ideal.  The default values for minimum image size is 1800x2200 and can be set with either
  * org.icepdf.core.imageReference.blurred.minwidth or org.icepdf.core.imageReference.blurred.minheight.  However
- * smaller values can lead  to missing content on low res images.
+ * smaller values can lead to missing content on low res images.
+ *
+ * When an image size is less then the min width and min height then the image data will then be passed onto the smooth
+ * scaled image reference implementation.  The SmoothScaledImageReference does a better job of smoothing out small
+ * images and printer bands.  For more information {@link SmoothScaledImageReference}.
  *
  * @since 6.2.4
  */
@@ -69,10 +73,23 @@ public class BlurredImageReference extends CachedImageReference {
         BufferedImage image = null;
         long start = System.nanoTime();
         try {
+            if (logger.isLoggable(Level.FINEST)) {
+                logger.finest("Starting blurred image reference processing. ");
+            }
             image = imageStream.getImage(graphicsState, resources);
+            // check constraints for applying a the kernel blur effect.
             if (image.getWidth() > minWidth && image.getHeight() > minHeight) {
                 BufferedImageOp op = new ConvolveOp(new Kernel(dimension, dimension, matrix));
                 image = op.filter(image, null);
+            }
+            // we fall back on try smooth scaled which may work better for smaller or banded images.
+            // this is also a similar effect
+            else {
+                if (logger.isLoggable(Level.FINEST)) {
+                    logger.finest("Falling back on smooth scaled image reference processing. ");
+                }
+                image = new SmoothScaledImageReference(
+                        imageStream, graphicsState, resources, imageIndex, parentPage).call();
             }
         } catch (Throwable e) {
             logger.log(Level.WARNING, "Error loading image: " + imageStream.getPObjectReference() +
