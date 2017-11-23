3544f34997ea88430a1ec343acf66a377cff1604
Patrick Corless <pcorless@gmail.com>
2017-11-23 19:35:28 +0000

    PDF-1100 qa updates for different document api's.

---
+++ icepdf/viewer/src/org/icepdf/ri/util/qa/CaptureTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2006-2017 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.icepdf.ri.util.qa;
+
+import java.nio.file.Path;
+
+public interface CaptureTest<T> {
+
+    int load(Path filePath);
+
+    T capture(int pageNumber, int renderHintType, final int boundary, float userRotation, float userZoom);
+
+    void dispose();
+}
---
+++ icepdf/viewer/src/org/icepdf/ri/util/qa/PageCapture.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2006-2017 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.icepdf.ri.util.qa;
+
+import org.icepdf.core.exceptions.PDFException;
+import org.icepdf.core.exceptions.PDFSecurityException;
+import org.icepdf.core.pobjects.Document;
+import org.icepdf.ri.util.FontPropertiesManager;
+import org.icepdf.ri.util.PropertiesManager;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ResourceBundle;
+
+public class PageCapture implements CaptureTest<BufferedImage> {
+
+    protected Document document;
+
+    @Override
+    public int load(Path filePath) {
+
+        // initiate font Cache manager, reads system if necessary,  load the cache otherwise.
+        // read/store the font cache.
+        ResourceBundle messageBundle = ResourceBundle.getBundle(
+                PropertiesManager.DEFAULT_MESSAGE_BUNDLE);
+        PropertiesManager properties = new PropertiesManager(System.getProperties(),
+                ResourceBundle.getBundle(PropertiesManager.DEFAULT_MESSAGE_BUNDLE));
+        new FontPropertiesManager(properties, System.getProperties(), messageBundle);
+
+        try {
+            document = new Document();
+            document.setFile(filePath.toAbsolutePath().toString());
+            return document.getNumberOfPages();
+        } catch (PDFException e) {
+            e.printStackTrace();
+        } catch (PDFSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    @Override
+    public BufferedImage capture(int pageNumber, int renderHintType, int boundary, float userRotation, float userZoom) {
+        try {
+            return (BufferedImage) document.getPageImage(pageNumber, renderHintType, boundary, userRotation, userZoom);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // capture the page.
+        return null;
+    }
+
+    @Override
+    public void dispose() {
+        document.dispose();
+    }
+}
