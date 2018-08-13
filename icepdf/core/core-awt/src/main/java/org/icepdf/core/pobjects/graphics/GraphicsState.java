61d897432816ba77f2f340e138be24eb38e7d170
Patrick Corless <pcorless@gmail.com>
2018-08-13 11:26:18 +0000

    PDF-1266 reverted previous experiment, will revisit Q/q, ca/CA state.

--- icepdf/core/core-awt/src/main/java/org/icepdf/core/pobjects/graphics/GraphicsState.java
+++ icepdf/core/core-awt/src/main/java/org/icepdf/core/pobjects/graphics/GraphicsState.java
@@ -558,11 +558,11 @@ public class GraphicsState {
             parentGraphicState.shapes.add(new ColorDrawCmd(parentGraphicState.getFillColor()));
 
             // apply the old alpha fill, removed as we need to guarantee the stack is in the correct state.
-//            parentGraphicState.shapes.add(new AlphaDrawCmd(
-//                    AlphaComposite.getInstance(parentGraphicState.getAlphaRule(), parentGraphicState.getFillAlpha())));
-//
-//            parentGraphicState.shapes.add(new AlphaDrawCmd(
-//                    AlphaComposite.getInstance(parentGraphicState.getAlphaRule(), parentGraphicState.getStrokeAlpha())));
+            parentGraphicState.shapes.add(new AlphaDrawCmd(
+                    AlphaComposite.getInstance(parentGraphicState.getAlphaRule(), parentGraphicState.getFillAlpha())));
+
+            parentGraphicState.shapes.add(new AlphaDrawCmd(
+                    AlphaComposite.getInstance(parentGraphicState.getAlphaRule(), parentGraphicState.getStrokeAlpha())));
             // stroke Color
 //            parentGraphicState.shapes.add(parentGraphicState.getStrokeColor());
         }
