13ead7422d04d173f44ea2800f00766aad73bd5a
Patrick Corless <pcorless@gmail.com>
2018-11-29 03:34:52 +0000

    PDF-1280 addition of annotation callback call for pageAnnotationsInitialize when page is loaded by PageViewComponentImpl.

--- icepdf/viewer/viewer-awt/src/main/java/org/icepdf/ri/common/views/PageViewComponentImpl.java
+++ icepdf/viewer/viewer-awt/src/main/java/org/icepdf/ri/common/views/PageViewComponentImpl.java
@@ -479,6 +479,8 @@ public class PageViewComponentImpl extends AbstractPageViewComponent implements 
     private void initializeAnnotationsComponent(Page page) {
         List<Annotation> annotations = page.getAnnotations();
         AbstractPageViewComponent parent = this;
+        // check if we are on the awt thread for the callback call.
+        documentViewController.getAnnotationCallback().pageAnnotationsInitialized(page);
         if (annotations != null && annotations.size() > 0) {
             // we don't want to re-initialize the component as we'll
             // get duplicates if the page has be gc'd
