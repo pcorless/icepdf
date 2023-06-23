522c0b7202b4d1845e5cfb1dec1a5c924de4d44e
Patrick Corless <patrick.corless@arterys.com>
2023-06-22 22:22:38 -0600

    GH-279 fix two page layout issue with odd number pages skipping last page layout

--- viewer/viewer-awt/src/main/java/org/icepdf/ri/common/views/TwoPageView.java
+++ viewer/viewer-awt/src/main/java/org/icepdf/ri/common/views/TwoPageView.java
@@ -104,7 +104,8 @@ public class TwoPageView extends AbstractDocumentView {
             if (viewAlignment == RIGHT_VIEW &&
                     ((index > 0 && index % 2 == 0) || (index > 0 && docLength == 2))) {
                 index--;
-            } else if ((index > 0 && index % 2 != 0) || (index > 0 && docLength == 2)) {
+            } else if (viewAlignment == LEFT_VIEW &&
+                    (index > 0 && index % 2 != 0) || (index > 0 && docLength == 2)) {
                 index--;
             }
 
