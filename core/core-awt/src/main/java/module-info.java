01e1012276afa8ea8e5208e2242f1d26029177a0
Patrick Corless <pcorless@users.noreply.github.com>
2024-05-07 22:04:41 -0600

    GH-353 add module support to the library (#354)

---
+++ core/core-awt/src/main/java/module-info.java
@@ -0,0 +1,49 @@
+module org.icepdf.core {
+    requires java.logging;
+    requires java.prefs;
+    requires java.desktop;
+    requires java.net.http;
+    requires java.naming;
+
+    requires org.bouncycastle.pkix;
+    requires org.bouncycastle.provider;
+    requires org.bouncycastle.util;
+
+    // automatic module names
+    requires org.apache.fontbox;
+    requires org.apache.pdfbox.jbig2;
+    requires org.apache.commons.logging;
+    requires com.twelvemonkeys.common.image;
+    requires com.twelvemonkeys.common.io;
+    requires com.twelvemonkeys.common.lang;
+    requires com.twelvemonkeys.imageio.core;
+    requires com.twelvemonkeys.imageio.metadata;
+    requires com.twelvemonkeys.imageio.tiff;
+
+    // core api
+    exports org.icepdf.core;
+    exports org.icepdf.core.application;
+    exports org.icepdf.core.events;
+    exports org.icepdf.core.exceptions;
+    exports org.icepdf.core.io;
+    exports org.icepdf.core.pobjects;
+    exports org.icepdf.core.pobjects.acroform;
+    exports org.icepdf.core.pobjects.acroform.signature;
+    exports org.icepdf.core.pobjects.acroform.signature.certificates;
+    exports org.icepdf.core.pobjects.acroform.signature.exceptions;
+    exports org.icepdf.core.pobjects.actions;
+    exports org.icepdf.core.pobjects.annotations;
+    exports org.icepdf.core.pobjects.fonts;
+    exports org.icepdf.core.pobjects.graphics;
+    exports org.icepdf.core.pobjects.graphics.commands;
+    exports org.icepdf.core.pobjects.graphics.images;
+    exports org.icepdf.core.pobjects.graphics.images.references;
+    exports org.icepdf.core.pobjects.graphics.text;
+    exports org.icepdf.core.pobjects.security;
+    exports org.icepdf.core.pobjects.structure;
+    exports org.icepdf.core.search;
+    exports org.icepdf.core.util;
+    exports org.icepdf.core.util.loggers;
+    exports org.icepdf.core.util.updater;
+
+}
---
+++ viewer/viewer-awt/src/main/java/module-info.java
@@ -0,0 +1,47 @@
+module org.icepdf.ri.viewer {
+    requires java.logging;
+    requires java.prefs;
+    requires java.desktop;
+    requires java.net.http;
+
+    requires org.bouncycastle.pkix;
+    requires org.bouncycastle.provider;
+    requires org.bouncycastle.util;
+
+    requires org.icepdf.core;
+
+    // viewer ri api
+    exports org.icepdf.ri.common;
+    exports org.icepdf.ri.common.fonts;
+    exports org.icepdf.ri.common.preferences;
+    exports org.icepdf.ri.common.print;
+    exports org.icepdf.ri.common.properties;
+    exports org.icepdf.ri.common.search;
+    exports org.icepdf.ri.common.tools;
+    exports org.icepdf.ri.common.utility.annotation;
+    exports org.icepdf.ri.common.utility.annotation.acroform;
+    exports org.icepdf.ri.common.utility.annotation.destinations;
+    exports org.icepdf.ri.common.utility.annotation.markup;
+    exports org.icepdf.ri.common.utility.annotation.properties;
+    exports org.icepdf.ri.common.utility.attachment;
+    exports org.icepdf.ri.common.utility.layers;
+    exports org.icepdf.ri.common.utility.outline;
+    exports org.icepdf.ri.common.utility.search;
+    exports org.icepdf.ri.common.utility.signatures;
+    exports org.icepdf.ri.common.utility.thumbs;
+    exports org.icepdf.ri.common.views;
+    exports org.icepdf.ri.common.views.annotations;
+    exports org.icepdf.ri.common.views.annotations.acroform;
+    exports org.icepdf.ri.common.views.annotations.signatures;
+    exports org.icepdf.ri.common.views.annotations.summary;
+    exports org.icepdf.ri.common.views.destinations;
+    exports org.icepdf.ri.common.views.listeners;
+    exports org.icepdf.ri.common.widgets;
+    exports org.icepdf.ri.common.widgets.annotations;
+    exports org.icepdf.ri.images;
+    exports org.icepdf.ri.util;
+    exports org.icepdf.ri.util.font;
+    exports org.icepdf.ri.util.qa;
+    exports org.icepdf.ri.viewer;
+
+}
