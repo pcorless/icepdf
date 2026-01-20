8a0e9adc48366e42b9ae59241a5ebed336314786
Patrick Corless <pcorless@gmail.com>
2026-01-19 20:11:45 -0700

    GH-457 code cleanup

--- viewer/viewer-awt/src/main/java/org/icepdf/ri/common/views/annotations/signing/SignatureCreationDialog.java
+++ viewer/viewer-awt/src/main/java/org/icepdf/ri/common/views/annotations/signing/SignatureCreationDialog.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026 Patrick Corless
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.icepdf.ri.common.views.annotations.signing;
 
 import org.bouncycastle.asn1.x500.X500Name;
@@ -122,7 +137,7 @@ public class SignatureCreationDialog extends EscapeJDialog implements ActionList
             Library library = signatureWidgetAnnotation.getLibrary();
             SignatureManager signatureManager = library.getSignatureDictionaries();
 
-            if (signatureManager.hasPermissionToSignDocument()) {
+            if (!signatureManager.hasPermissionToSignDocument()) {
                 logger.warning("No signing permissions for this document.");
                 JOptionPane.showMessageDialog(this,
                         messageBundle.getString("viewer.annotation.signature.creation.dialog.permission.error.msg"),
