6668bef898b37fc43ec5de97becd1fe8e3d949d0
Patrick Corless <pcorless@users.noreply.github.com>
2026-02-26 21:41:43 -0700

    GH-469 update license headers (#470)

--- core/core-awt/src/main/java/org/icepdf/core/util/parser/content/ContentParser.java
+++ core/core-awt/src/main/java/org/icepdf/core/util/parser/content/ContentParser.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 Patrick Corless
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.icepdf.core.util.parser.content;
 
 import org.icepdf.core.pobjects.*;
@@ -846,7 +862,7 @@ public class ContentParser extends AbstractContentParser {
                     // Sets the line dash pattern in the graphics state. A normal line
                     // is [] 0.  See Graphics State -> Line dash patter for more information
                     // in the PDF Reference.  Java 2d uses the same notation so there
-                    // is not much work to be done other then parsing the data.
+                    // is not much work to be done other than parsing the data.
                     case Operands.d:
                         consume_d(graphicState, stack, shapes);
                         break;
@@ -1080,4 +1096,3 @@ public class ContentParser extends AbstractContentParser {
         }
     }
 }
-
